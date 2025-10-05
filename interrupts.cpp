be25283ffa89a102be500403a1392a1d359bda75
Satari4545 <yousatari@gmail.com>
2025-10-05 01:16:50 -0400

    interrupt code with first example and execution file

--- interrupts.cpp
+++ interrupts.cpp
@@ -20,10 +20,9 @@ int main(int argc, char** argv) {
 
     /******************ADD YOUR VARIABLES HERE*************************/
     
-    int current_time = 0;           // Track current simulation time
-    int context_save_time = 10;     // Time to save/restore context
+    int time = 0;
+    int context_time = 10;
     
-    //test test test yousif
     /******************************************************************/
 
     //parse each line of the input trace file
@@ -32,7 +31,63 @@ int main(int argc, char** argv) {
 
         /******************ADD YOUR SIMULATION CODE HERE*************************/
         
-       
+        // Skip if activity is not understood
+        if (activity == "null" || duration_intr == -1) {
+            continue;
+        }
+
+        // if activity is CPU 
+        if (activity == "CPU") {
+            execution += std::to_string(time) + ", " + std::to_string(duration_intr) + 
+                        ", CPU Burst\n";
+            time += duration_intr;
+        }
+        // if activity is SYSCALL
+        else if (activity == "SYSCALL") {
+            int device_id = duration_intr;
+            
+            // Use helper function for interrupt overhead
+            auto [interrupt_steps, updated_time] = intr_boilerplate(time, device_id, context_time, vectors);
+            execution += interrupt_steps;
+            time = updated_time;
+
+            // Run the device driver
+            execution += std::to_string(time) + ", " + std::to_string(40) + 
+                        ", SYSCALL: run the ISR (device driver)\n";
+            time += 40;
+
+            // Transfer data between device and memory
+            execution += std::to_string(time) + ", " + std::to_string(40) + 
+                        ", transfer data from device to memory\n";
+            time += 40;
+
+            // Check for errors 
+            int remaining_time = delays[device_id] - 80;
+            execution += std::to_string(time) + ", " + std::to_string(remaining_time) + 
+                        ", check for errors\n";
+            time += remaining_time;
+        }
+        // if activity is END_IO
+        else if (activity == "END_IO") {
+            int device_id = duration_intr;
+            
+            // Use helper function for interrupt overhead
+            auto [interrupt_steps, updated_time] = intr_boilerplate(time, device_id, context_time, vectors);
+            execution += interrupt_steps;
+            time = updated_time;
+
+            // Run the device driver
+            execution += std::to_string(time) + ", " + std::to_string(40) + 
+                        ", ENDIO: run the ISR (device driver)\n";
+            time += 40;
+
+            // Check device status 
+            int remaining_time = delays[device_id] - 40;
+            execution += std::to_string(time) + ", " + std::to_string(remaining_time) + 
+                        ", check device status\n";
+            time += remaining_time;
+        }
+        
         /************************************************************************/
 
     }
